4f249c7a0581f30eac97bb0c76c532ccd6ec035a
prajwold <prajwol_dangol@hotmail.com>
2014-08-27 15:28:20 +0545

    prajwol template installed

--- templates/nepaltelivision/index.php
+++ templates/nepaltelivision/index.php
@@ -121,7 +121,13 @@ else
 	<![endif]-->
 </head>
 
-<body>
+<body class="site <?php echo $option
+	. ' view-' . $view
+	. ($layout ? ' layout-' . $layout : ' no-layout')
+	. ($task ? ' task-' . $task : ' no-task')
+	. ($itemid ? ' itemid-' . $itemid : '')
+	. ($params->get('fluidContainer') ? ' fluid' : '');
+?>">
 
 	<!-- Body -->
 	<div class="body">
