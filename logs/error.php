da168c68268e1442f7b02f7d6c227aaa84bf5235
featherwebs <info.akashshrestha@gmail.com>
2014-08-27 11:29:58 -0700

    joomla ntv all files

---
+++ logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-08-21 20:14:57 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-08-21T20:14:57+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-08-21T20:15:07+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-08-21T20:16:53+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
---
+++ logs/joomla_update.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-08-21 20:22:45 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-08-21T20:22:45+00:00	INFO	update	Update started by user Super User (680). Old version is 3.3.0.
+2014-08-21T20:22:45+00:00	INFO	update	Downloading update file from .
+2014-08-21T20:23:49+00:00	INFO	update	File Joomla_3.3.x_to_3.3.3-Stable-Patch_Package.zip successfully downloaded.
+2014-08-21T20:23:49+00:00	INFO	update	Starting installation of new version.
+2014-08-21T20:24:18+00:00	INFO	update	Finalising installation.
+2014-08-21T20:24:18+00:00	INFO	update	Deleting removed files and folders.
+2014-08-21T20:24:28+00:00	INFO	update	Cleaning up after installation.
+2014-08-21T20:24:28+00:00	INFO	update	Update to version 3.3.3 is complete.
